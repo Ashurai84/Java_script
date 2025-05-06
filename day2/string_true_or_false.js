36d298002123aa3412780cf02ccf22d439be9ab5
Ashurai84 <raia40094@gmail.com>
2025-05-06 14:29:17 +0530

    uses of argument only once

--- day2/string_true_or_false.js
+++ day2/string_true_or_false.js
@@ -15,11 +15,28 @@
    
   
 
-let arr = ["hello", " IS " ," THIS" ,"BMW"]
-function findindexinarray (find){
-  console.log(find)
+// let arr = ["hello", " IS " ," THIS" ,"BMW"]
+// function findindexinarray (find){
+//   console.log(find)
+// }
+//  f1= prompt("Enter the  array  to show ")
+//  console.log(f1)
+// console.log(arr.indexOf(f1))
+
+
+
+
+// In Javascript if we declare only one argument in a function and gives n number of inputs it will ignore all the inputs and take only first one. It does not throw any error.
+// eg:-
+function demo(num1,num2,num3){
+  console.log(num1)
 }
- f1= prompt("Enter the  array  to show ")
- console.log(f1)
-console.log(arr.indexOf(f1))
+console.log(demo(2,3,4))
+
+function demo2(...num1)
+{
+  console.log(num1)
+
+}
+demo2(2,3,4,5,6,7)
 
