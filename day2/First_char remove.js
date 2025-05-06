6b67eaba9fe42119931ecbe35ef1626d39085a2d
Ashurai84 <raia40094@gmail.com>
2025-05-06 12:07:53 +0530

    here will show where  the both input is same or not , and   find the array  of index

---
+++ day2/First_char remove.js
@@ -0,0 +1,6 @@
+function fristcharremove(str1){
+    return str1.slice(1);}
+  
+    let first_input = prompt("Enter the word to remove first word ")
+    console.log(first_input)
+    console.log(fristcharremove(first_input))
---
+++ day2/string_true_or_false.js
@@ -0,0 +1,25 @@
+// function strings_are_same(str1 , str2 ){
+//   return str1==str2;}
+
+
+//   let first_input = prompt("Enter the first string");
+//   let second_input = prompt("enter the second value :")
+
+//   let result_be = strings_are_same(first_input , second_input)
+
+//   console.log("Are the number are same ", result_be)
+
+
+
+ 
+   
+  
+
+let arr = ["hello", " IS " ," THIS" ,"BMW"]
+function findindexinarray (find){
+  console.log(find)
+}
+ f1= prompt("Enter the  array  to show ")
+ console.log(f1)
+console.log(arr.indexOf(f1))
+
