d0845eb9bd850952a929a729cc56e8d606264d6f
Ashurai84 <raia40094@gmail.com>
2025-05-08 12:36:00 +0530

    here we are done with protoyping and opps concept

--- day4/prototype.js
+++ day4/prototype.js
@@ -1,15 +1,22 @@
 function person (name , age , loc ){
-    this.name=name;
+    this.function=name;
     this.age=age;
     this.loc=loc;
-
+    this.pname = function () {
+        console.log("hi")
+    };
 }
-
 let p1 = new person("ashu" , 87, "new")
+console.log(p1.pname);
 
 p1.name="ashutosh"
 console.log(p1)
 p1.age=19
 
 person.prototype.hehe="hehe"
+person.prototype.java="hllo"
+
 console.log(p1)
+// console.log(pname.java)
+
+
